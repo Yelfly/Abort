7b25158a38c5606627369e109cdea1884bb0d374
yelfly <yelfly@163.com>
2021-11-11 10:49:08 +0800

    1.增加Swagger支持

--- cloud-provider-payment8001/src/main/java/com/yelfly/springdemo/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yelfly/springdemo/PaymentMain8001.java
@@ -2,9 +2,10 @@ package com.yelfly.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class PaymentMain8001 {
     public static void main(String[] args){
         SpringApplication.run(PaymentMain8001.class,args);
---
+++ cloud-provider-payment8001/src/main/java/com/yelfly/springdemo/config/SwaggerConfig.java
@@ -0,0 +1,73 @@
+package com.yelfly.springdemo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Slf4j
+@EnableSwagger2
+@Configuration
+//Profile是方法1（只在dev和test环境下开启）
+@Profile({"dev", "test"})
+public class SwaggerConfig implements WebMvcConfigurer {
+    //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+    //这个是方法2哦，使用的话在new Docket里添加.Enable方法将参数放入即可
+    @Value(value = "${swagger.show}")
+    private Boolean swaggerEnabled;
+
+    /**
+     * apiInfo() 增加API相关信息
+     * 所有的注解
+     * .apis(RequestHandlerSelectors.any())
+     * 指定部分注解1.Api.class(@APi),2.ApiOperation.class(@ApiOperation),3.ApiImplicitParam.class(@ApiImplicitParam)
+     * .apis(RequestHandlerSelectors.withMethodAnnotation(Api.class))
+     * 指定包路径
+     * .apis(RequestHandlerSelectors.basePackage("这里填写需要的路径"))
+     * .paths() 这个是包路径下的路径,PathSelectors.any()是包下所有路径
+     */
+    @Bean
+    public Docket createRestApi() {
+        log.info("" + swaggerEnabled);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                //创建
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SpringBoot-Swagger2集成")
+                .description("springboot | swagger")
+                // 作者信息
+                .contact(new Contact("kacen", "https://www.baidu.com", "abc@qq.com"))
+                .version("0.0.1")
+                .build();
+    }
+
+    //这个是可要可不要的，具体看需求
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
